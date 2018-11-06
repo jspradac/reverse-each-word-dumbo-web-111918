0b3ea4cf37f66dbe9314e3a5d0940f0b22670db0
Samuel Prada <jspradac@gmail.com>
2018-11-06 20:50:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(string)
   a = string.split(" ")
-  reversed =[]
-  a.each do | word |
-    reversed.push(word.reverse)
+  a.collect do | word |
+    word.reverse
   end 
-  reversed.join(" ")
 end 
